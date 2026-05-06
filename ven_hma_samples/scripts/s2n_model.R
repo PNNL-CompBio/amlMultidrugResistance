762de75385143d3d28410e227eb0b6c96b692ca5
CamiloPosso <82610716+CamiloPosso@users.noreply.github.com>
2026-05-06 10:52:31 -0700

    Fixing bugs

--- ven_hma_samples/scripts/s2n_model.R
+++ ven_hma_samples/scripts/s2n_model.R
@@ -274,6 +274,230 @@ eln_model <- function(msnset, response, pred.cls, alpha){
 }
 
 
+## Modification of MSnSetUtils function.
+plot_pca <- function(eset, phenotype = NULL, shape = NULL, label = NULL, z_score = TRUE,
+                     princomp_center = TRUE, show_ellipse = TRUE, components = 1:2, biplot = FALSE,
+                     biplot_labels = NULL, standardize = TRUE, save_dfs = NULL,
+                     num_features = 6L, show_NA = TRUE, label_size = 3, output_type = NA,
+                     legend_title = phenotype,
+                     arrow_args = list(), label_args = list(), ...) {
+   
+   # Handling coloring by phenotype. Do this first, in case
+   # rows are removed when show_NA = FALSE
+   if (!is.null(phenotype)) {
+      colorBy <- pData(eset)[, phenotype]
+      # If not showing missing values, remove those samples
+      if (!show_NA) {
+         idx <- !is.na(colorBy)
+         eset <- eset[, idx]
+         colorBy <- colorBy[idx]
+      }
+   } else {
+      show_ellipse <- FALSE
+      colorBy <- NULL
+   }
+   if (!is.null(shape)){
+      shapeBy <- pData(eset)[, shape]
+      if (!show_NA) {
+         idx <- !is.na(shapeBy)
+         eset <- eset[, idx]
+         shapeBy <- shapeBy[idx]
+      }
+   } else {
+      shapeBy <- NULL
+   }
+   
+   # Check that components are valid
+   if (length(components) != 2) {
+      stop(sprintf("components must be a vector of length 2, not %d.",
+                   length(components)))
+   }
+   if (!all(components %in% 1:ncol(eset))) {
+      stop(sprintf("The values of components must be between 1 and %d.",
+                   ncol(eset)))
+   }
+   
+   complete_rows <- complete.cases(exprs(eset))
+   
+   # Check that there are enough complete rows for PCA
+   if (sum(complete_rows) < 2) {
+      stop("There are fewer than 2 rows with non-missing data.")
+   }
+   
+   message(sprintf("Subsetting to %d complete rows for PCA.",
+                   sum(complete_rows)))
+   
+   # Subset to complete rows
+   eset <- eset[complete_rows, ]
+   
+   # If z_score, convert to Z-Scores by sample (row when transposed)
+   if (z_score) {
+      z <- t(scale(exprs(eset), center = TRUE, scale = TRUE))
+   } else {
+      z <- t(exprs(eset))
+   }
+   
+   ## PCA
+   # By default, center = TRUE, scale. = FALSE
+   pca_res <- prcomp(z, center = princomp_center)
+   
+   u <- pca_res$x # Scores
+   v <- pca_res$rotation # Eigenvectors
+   
+   if (standardize) {
+      n <- nrow(u)
+      lam <- pca_res$sdev * sqrt(n)
+      
+      # Scale u down and v up. Product is still the same
+      u <- t(t(u) / lam)
+      v <- t(t(v) * lam)
+   }
+   
+   # Determine ratio between scale of v and u
+   u_range <- apply(u[, components], 2, function(x) abs(range(x)))
+   v_range <- apply(v[, components], 2, function(x) abs(range(x)))
+   
+   ratio <- max(v_range / u_range) # ratio for scaling v and secondary axes
+   v <- v / ratio # scale v
+   
+   if (!is.null(save_dfs)){
+      u <- u %>% as.data.frame()
+      # If colorBy is not NULL, add that column to df
+      if (!is.null(colorBy)) {
+         u$colorBy <- colorBy
+      }
+      if (!is.null(shapeBy)) {
+         u$shapeBy <- shapeBy
+      }
+      
+      assign(save_dfs, list("sample_decomposition" = u, "feature_decomposition" = v %>% as.data.frame() %>%
+                               mutate(feature = rownames(.))), envir = globalenv())
+   }
+   
+   # Data frames for plotting
+   df.u <- as.data.frame(u[, components])
+   df.v <- as.data.frame(v[, components])
+   
+   # Percent of variance explained by each PC
+   d <- pca_res$sdev # Standard deviations
+   var_expl <- round(100 * d ^ 2 / sum(d ^ 2), digits = 2)[components]
+   axis_labs <- sprintf("PC%d (%g%%)", #"%sPC%d (%g%%)",
+                        # ifelse(obs.scale == 0, "Standardized ", ""),
+                        components,
+                        var_expl)
+   
+   # If colorBy is not NULL, add that column to df
+   if (!is.null(colorBy)) {
+      df.u$colorBy <- colorBy
+   }
+   if (!is.null(shapeBy)) {
+      df.u$shapeBy <- shapeBy
+   }
+   
+   ## Visualization
+   # Base plot
+   p <- ggplot(data = df.u, mapping = aes(x = df.u[, 1], y = df.u[, 2], color = colorBy, shape = shapeBy)) +
+      geom_hline(yintercept = 0, lty = "longdash", color = "darkgrey") +
+      geom_vline(xintercept = 0, lty = "longdash", color = "darkgrey") +
+      labs(x = axis_labs[1], y = axis_labs[2]) +
+      theme_bw() +
+      theme(aspect.ratio = 1)
+   
+   # 50% confidence ellipse layer first so they are
+   # beneath the layer of points or labels.
+   if (show_ellipse & !is.numeric(colorBy)) {
+      p <- p +
+         stat_ellipse(mapping = aes(fill = colorBy, color = NULL),
+                      geom = "polygon", type = "norm",
+                      level = 0.5, alpha = 0.1, show.legend = TRUE)
+   }
+   
+   # If label is NULL, add points. Otherwise, add labels
+   if (is.null(label)) {
+      p <- p +
+         geom_point(...)
+   } else {
+      labels <- pData(eset)[, label]
+      p <- p + geom_point(...) + 
+         ggrepel::geom_label_repel(mapping = aes(label = labels), 
+                                   size = label_size, ...)
+   }
+   
+   # Set titles for color and fill legend
+   p <- p +
+      guides(color = guide_legend(title = legend_title),
+             fill = guide_legend(title = legend_title))
+   
+   # If colorBy is numeric, use a colorbar
+   if (is.numeric(colorBy)) {
+      p <- p +
+         guides(color = guide_colorbar(title = legend_title))
+   }
+   
+   ## Biplot
+   if (biplot) {
+      # Get the indices of the top influential features
+      # from each principal component. num_features determines how
+      # many to select from each component.
+      top_features <- lapply(1:2, function(i) {
+         order(abs(df.v)[, i], decreasing = TRUE)[1:num_features]
+      })
+      top_features <- unique(unlist(top_features))
+      
+      # Subset loadings to top features and rename columns
+      df.v <- df.v[top_features, ]
+      colnames(df.v) <- c("xend", "yend")
+      df.v$x <- df.v$y <- 0
+      
+      # If biplot_labels is not provided, default to row names
+      if (is.null(biplot_labels)) {
+         df.v$labels <- rownames(df.v)
+      } else {
+         df.v$labels <- fData(eset)[top_features, biplot_labels]
+      }
+      
+      scale_args <- list(expand = expansion(mult = rep(0.1, 2)),
+                         sec.axis = sec_axis(~ . * ratio))
+      
+      # Arguments for geom_segment
+      arrow_args <- list(mapping = aes(x = x, y = y, xend = xend, yend = yend),
+                         arrow = arrow(length = unit(0.5, "line")),
+                         data = df.v, color = "red3") %>%
+         # Allow user-supplied args to overwrite defaults
+         modifyList(val = arrow_args, keep.null = TRUE)
+      
+      # Arguments for geom_label_repel
+      label_args <- list(mapping = aes(x = xend, y = yend, label = labels),
+                         data = df.v,
+                         color = arrow_args[["color"]],
+                         max.overlaps = Inf,
+                         min.segment.length = 0,
+                         fill = alpha("white", 0.5)) %>%
+         # Allow user-supplied args to overwrite defaults
+         modifyList(val = label_args, keep.null = TRUE)
+      
+      # Add segments with arrows and text labels
+      p <- p +
+         # Add extra padding around plot area and secondary axes for v units
+         do.call(scale_x_continuous, scale_args) +
+         do.call(scale_y_continuous, scale_args) +
+         do.call(geom_segment, arrow_args) +
+         do.call(geom_label_repel, label_args) +
+         theme(axis.text.y.right = element_text(color = arrow_args[["color"]]),
+               axis.text.x.top = element_text(color = arrow_args[["color"]]),
+               axis.ticks.y.right = element_line(color = arrow_args[["color"]]),
+               axis.ticks.x.top = element_line(color = arrow_args[["color"]]))
+   }
+   
+   if (output_type == "full"){
+      return(list("plot" = p, "data.u" = as.data.frame(u[, ]), "data.v" = as.data.frame(v[, ]),
+                  "axis_labs" = axis_labs))
+   } else{
+      return(p)
+   }
+}
+
+
 ###SG: added this from ../../drug_treated_samples
 
 diffexp_helper <- function(m, contrast_var, contrasts){
